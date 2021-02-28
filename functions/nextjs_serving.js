f251f83f343f2ec9f7d72fde0af883aa0ca861bf
Tim Cook <therealtimcook@gmail.com>
2021-02-27 22:24:05 -0500

    Maybe stop using yarn

--- functions/nextjs_serving.js
+++ functions/nextjs_serving.js
@@ -1,14 +1,20 @@
+const { join } = require('path');
 const { https } = require('firebase-functions');
 const { default: next } = require('next');
+
+const nextjsDistDir = join("..", require('../next.config.js').distDir);
 
 const isDev = process.env.NODE_ENV !== 'production';
 
 const server = next({
   dev: isDev,
-  conf: { distDir: '.next' },
+  conf: {
+    distDir: nextjsDistDir
+  },
 });
 
 const nextjsHandle = server.getRequestHandler();
-exports.nextServer = https.onRequest((req, res) => {
+
+exports.nextjsServing = https.onRequest((req, res) => {
   return server.prepare().then(() => nextjsHandle(req, res));
 });
