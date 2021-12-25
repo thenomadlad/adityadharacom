b708691ab68259509bb5ad863b9093ea0e4949e6
Aditya Dhara <adhara@wayfair.com>
2021-12-24 22:15:43 -0500

    Adding a hand wave coz why not

---
+++ src/lib/components/waving_hand.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+	const waveInterval = 10000;
+	let wave = false;
+
+	function setWave() {
+		wave = true;
+	}
+
+	function setNotWave() {
+		wave = false;
+	}
+
+	// kick off the wave
+	function wavePeriodically() {
+		if (!wave) {
+			setWave();
+			setTimeout(() => {
+				setNotWave();
+			}, 500);
+		}
+		setTimeout(wavePeriodically, waveInterval);
+	}
+	setTimeout(wavePeriodically, waveInterval);
+</script>
+
+<style>
+	.hand {
+		display: inline-block;
+	}
+	.waveOut {
+		transition: transform 0.5s ease-in;
+		transform: rotate(0deg)
+	}
+	.waveIn {
+		transition: transform 0.5s ease-in;
+		transform: rotate(15deg)
+	}
+</style>
+
+<div>
+    <h1
+        class="text-8xl m-10"
+        on:mouseover={setWave}
+        on:focus={setWave}
+        on:mouseout={setNotWave}
+        on:blur={setNotWave}
+    >
+        <span class={'hand ' + (wave ? 'waveIn' : 'waveOut')}>
+            👋
+        </span>
+        😊
+    </h1>
+</div>
