ad7bbbf742bcac879573a005120d41d9d5e2ebe4
Aditya Dhara <adharacapybara@gmail.com>
2022-03-07 04:43:36 -0500

    Upgrade packages and add blag

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Card from '$lib/components/card.svelte';
 	import WavingHand from '$lib/components/waving_hand.svelte';
 </script>
 
@@ -6,17 +7,23 @@
 	<title>Aditya makes things</title>
 </svelte:head>
 
-<div class="flex h-screen">
+<div class="flex">
 	<div class="m-auto inline-grid grid-rows-2 justify-center items-center">
 		<WavingHand />
 		<div class="inline-grid grid-cols-2 gap-x-4 text-3xl">
 			<a
 				class="text-center text-gray-400 hover:text-black"
-				href="https://www.linkedin.com/in/adityadhara/"><i class="fab fa-linkedin" /></a
+				href="https://www.linkedin.com/in/adityadhara/"
 			>
-			<a class="text-center text-gray-400 hover:text-black" href="https://github.com/thenomadlad"
-				><i class="fab fa-github" /></a
-			>
+				<i class="fab fa-linkedin" />
+			</a>
+			<a class="text-center text-gray-400 hover:text-black" href="https://github.com/thenomadlad">
+				<i class="fab fa-github" />
+			</a>
 		</div>
 	</div>
+</div>
+
+<div class="columns-3">
+	<Card />
 </div>
