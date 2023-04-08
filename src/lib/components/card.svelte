7bfea3aac7ba4f4da4a97e3bb126e1126af40cfd
Aditya Dhara <dev@adityadhara.com>
2023-04-08 15:03:16 -0400

    Add github repos on homepage

--- src/lib/components/card.svelte
+++ src/lib/components/card.svelte
@@ -1,21 +1,25 @@
 <script lang="ts">
-	export let image_src = '/favicon.png';
-	export let image_alt = 'Image alt';
+	import { format } from 'date-fns';
+
+	// export let image_src = '/favicon.png';
+	// export let image_alt = 'Image alt';
+	export let heading: string = 'What even is a heading?';
+	export let date: Date = new Date('2022-10-12');
+	
+	$: formattedDate = format(date, 'yyyy-MM-dd');
 </script>
 
-<div>
-	<div class="rounded border-2 overflow-hidden shadow-lg">
-		<img class="w-full border-b-2" src={image_src} alt={image_alt} />
-		<div class="px-6 py-4">
-			<div class="font-bold text-xl mb-2">
-				<slot name="heading">What even is a heading?</slot>
-			</div>
-			<p class="text-gray-700 text-base">
-				<slot name="description">Who needs a description amirite?</slot>
-			</p>
-		</div>
-		<div class="px-6 pt-4 pb-2">
-			<slot name="when">Does it matter when?</slot>
-		</div>
+<div class="flex flex-col justify-between rounded border-2 overflow-hidden shadow-lg h-full">
+	<!-- <img class="w-full border-b-2" src={image_src} alt={image_alt} /> -->
+	<div class="px-6 py-4 font-bold text-xl mb-2 text-center">
+		<slot name="heading">{heading}</slot>
+	</div>
+	<div class="px-6 mt-2 text-center">
+		<p class="text-gray-700 text-base">
+			<slot>Who needs a description amirite?</slot>
+		</p>
+	</div>
+	<div class="px-6 pt-4 pb-2 text-center text-slate-400">
+		<slot name="when">{formattedDate}</slot>
 	</div>
 </div>
