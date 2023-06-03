cc6cf5c469ce8d54555df40c46e188dbf544b6e5
Aditya Dhara <dev@adityadhara.com>
2023-06-03 13:58:48 -0400

    Added everything

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,4 @@
 	import '@fortawesome/fontawesome-free/css/all.css';
 </script>
 
-<main class="container mx-auto">
-	<slot />
-	<div class="flex align-center justify-center border-t-2 mt-10 mb-10">
-		<p class="text-slate-400">Woah you reached the end!</p>
-	</div>
-</main>
+<slot />
