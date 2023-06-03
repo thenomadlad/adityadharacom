114366b70a2f90be02d67ca21930ae20dae95b85
Aditya Dhara <dev@adityadhara.com>
2023-06-03 14:27:50 -0400

    Update playwright, hopefully that works

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,11 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
+import { PlaywrightTestConfig, defineConfig } from '@playwright/test';
 
-const config: PlaywrightTestConfig = {
+const config: PlaywrightTestConfig = defineConfig({
 	webServer: {
 		command: 'pnpm run build && pnpm run preview',
 		port: 4173
 	},
 	testDir: 'tests'
-};
+});
 
 export default config;
