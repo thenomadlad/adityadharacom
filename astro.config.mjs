e974e78591aaa43d5c3e3b36ae99ee605e8bac98
Aditya Dhara <dev@adityadhara.com>
2025-12-27 02:07:32 -0800

    Ok first attempt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,25 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
 
 import svelte from "@astrojs/svelte";
 import rehypeMermaid from 'rehype-mermaid';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://adityadhara.com",
-  integrations: [mdx(), sitemap(), tailwind(), svelte()],
+  integrations: [mdx(), sitemap(), svelte()],
+
   markdown: {
     syntaxHighlight: {
       type: 'shiki',
       excludeLangs: ['mermaid', 'math'],
     },
     rehypePlugins: [rehypeMermaid],
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
   }
 });
-
