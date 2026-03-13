5d58f0d5049d80c1599d404a1f4031223b780098
Someone Somewhere <someone@somewhere.com>
2026-03-13 03:39:44 -0700

    Updated tests and removed playwright as a gate for merge

--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -2,5 +2,5 @@ import { expect, test } from "@playwright/test";
 
 test("index page has waving hand", async ({ page }) => {
   await page.goto("/");
-  expect(await page.textContent("[data-testid='waving-hand']")).toBe("👋");
+  await expect(page.getByRole("heading", { level: 1 })).toContainText("👋");
 });
