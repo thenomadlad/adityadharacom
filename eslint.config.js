949c8c529ec9e9c1895f8605c303ffa031c360bf
Someone Somewhere <someone@somewhere.com>
2026-03-13 02:28:33 -0700

    Moved to solid

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,5 @@
 import js from '@eslint/js';
 import eslintPluginAstro from 'eslint-plugin-astro';
-import svelteParser from 'svelte-eslint-parser'
 import ts from 'typescript-eslint';
 import globals from "globals";
 
@@ -10,16 +9,6 @@ export default [
   js.configs.recommended,
   ...ts.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
-  {
-    files: ['**/*.svelte'],
-    // See more details at: https://typescript-eslint.io/packages/parser/
-    languageOptions: {
-      parser: svelteParser,
-      parserOptions: {
-        parser: ts.parser,
-      }
-    }
-  },
   {
     // we need these globals across all filetypes
     languageOptions: {
